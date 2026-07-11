583af4f617988338fc118a0ce183bdc08ca04099
AanchalRathi <aanchalrathiwork@gmail.com>
2026-07-11 13:05:42 +0530

    added validation checks before saving uploaded invoice

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, UploadFile, File
 from dotenv import load_dotenv
 from database import SessionLocal
 from models import Invoice
+from validation import validate_invoice
 from ocr import extract_text_from_image
 from extraction import extract_from_printed_text
 import shutil
@@ -32,11 +33,17 @@ async def upload_invoice(file: UploadFile = File(...)):
     # Gemini extraction
     extracted_data = extract_from_printed_text(raw_text)
     # save to DB
+    
     db = SessionLocal()
+
+    status, reason = validate_invoice(extracted_data, db, Invoice)
+    
     invoice = Invoice(
         filename=file.filename,
         raw_ocr_text=raw_text,
-        extracted_data=extracted_data
+        extracted_data=extracted_data,
+        validation_status=status,
+        validation_reason=reason
     )
     db.add(invoice)
     db.commit()
@@ -47,5 +54,8 @@ async def upload_invoice(file: UploadFile = File(...)):
 
     return {
         "invoice_id": invoice.id,
-        "extracted_data": extracted_data
+        "extracted_data": extracted_data,
+        "validation_status": status,
+        "validation_reason": reason
+
     }
