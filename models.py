6969752788602c360ee3240f16ce8e181969ee3f
AanchalRathi <aanchalrathiwork@gmail.com>
2026-07-12 12:54:50 +0530

    Switch extracted_data coloumn to JSONB to support duplicte check queries

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime, JSON
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy.dialects.postgresql import JSONB
 from datetime import datetime
 from database import Base
 
@@ -8,7 +9,7 @@ class Invoice(Base):
     id = Column(Integer, primary_key=True, index=True)
     filename = Column(String)
     raw_ocr_text = Column(String)
-    extracted_data = Column(JSON)          # structured JSON from Gemini
+    extracted_data = Column(JSONB)          # structured JSONB from Gemini
     validation_status = Column(String, default="pending")  # pending / valid / flagged
     validation_reason = Column(String, nullable=True)
     created_at = Column(DateTime, default=datetime.utcnow)
