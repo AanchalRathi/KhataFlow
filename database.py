1c5afe60932e3987b9b7f9f50d5fb35c2783bd73
AanchalRathi <aanchalrathiwork@gmail.com>
2026-07-21 14:34:54 +0530

    Switch to NullPool for more reliable connections with Neon's serverless pooling

--- database.py
+++ database.py
@@ -1,11 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.pool import NullPool
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL, pool_recycle=280)
+engine = create_engine(
+    DATABASE_URL,
+    poolclass=NullPool,
+    pool_pre_ping=True
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
